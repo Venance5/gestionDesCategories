8d8fd28e86176a484df6271d47f8fba56c9421f7
djabidja <djabia-venance.djabia@univ-rouen.fr>
2024-10-08 23:52:30 +0200

    creationBackend

---
+++ backend/src/main/java/univrouen/fr/GestionCategories/GestionCategoriesApplication.java
@@ -0,0 +1,13 @@
+package univrouen.fr.GestionCategories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCategoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCategoriesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/univrouen/fr/GestionCategories/GestionCategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package univrouen.fr.GestionCategories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCategoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
